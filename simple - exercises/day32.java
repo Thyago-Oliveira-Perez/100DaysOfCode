0e27d6536df2168ef919abd40efc3623b61c5534
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-02-04 20:51:05 -0300

    day32 - fixed

--- simple - exercises/day32.java
+++ simple - exercises/day32.java
@@ -16,30 +16,31 @@ public class day32 {
         System.out.println(MessageFormat.format("Classifications -> {0}", PlayerRank(rankedScores, new int[]{106, 107, 105})));
         System.out.println(MessageFormat.format("Classifications -> {0}", PlayerRank(rankedScores, new int[]{106, 95, 105})));
         System.out.println(MessageFormat.format("Classifications -> {0}", PlayerRank(rankedScores, new int[]{106, 92, 92})));
-        System.out.println(MessageFormat.format("Classifications -> {0}", PlayerRank(rankedScores, new int[]{70, 80, 105, 55})));
+        System.out.println(MessageFormat.format("Classifications -> {0}", PlayerRank(rankedScores, new int[]{55, 80, 105, 70})));
     }
 
     public static List<Integer> PlayerRank (List<Integer> rankedScores, int[] playerScores) {
 
         List<Integer> classifications = new ArrayList<>();
+        int[] scores = new int[playerScores.length];
 
-        int top1 = Collections.max(rankedScores);
-        int top2 = Collections.max(rankedScores.stream().filter(e -> e != top1).toList());
-        int top3 = Collections.max(rankedScores.stream().filter(e -> e != top1 && e != top2).toList());
-        int position = 3;
+        List<Integer> playerScoresList = new ArrayList<>();
+        for (int score : playerScores) playerScoresList.add(score);
+        Collections.sort(playerScoresList);
 
-        for (int score : playerScores) {
-            if (score >= top1) {
-                classifications.add(1);
-            } else if (score >= top2) {
-                classifications.add(2);
-            } else if (score >= top3) {
-                classifications.add(3);
-            } else {
-                classifications.add(position + 1);
-                position++;
+        for (int i = 0 ; i < playerScoresList.size() ; i++) {
+            for (int j = 0 ; j < playerScores.length ; j++) {
+                if (playerScores[j] == playerScoresList.get(i)) {
+                    if (playerScores[j] >= 100) {
+                        scores[j] = 1;
+                    } else {
+                        scores[j] = (playerScoresList.size() - i);
+                    }
+                }
             }
         }
+
+        for (int i : scores) classifications.add(i);
 
         return classifications;
     }
