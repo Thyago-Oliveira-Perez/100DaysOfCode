6026598d5597c948e37c099fa5eb3fd877b891d5
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-07-15 11:22:00 -0300

    init C# 100dayOfCode - day01

---
+++ C#/day-01/day-01/Program.cs
@@ -0,0 +1,29 @@
+﻿class Day01
+{
+    public static void Main()
+    {
+        Console.WriteLine("Type your age:");
+
+        try
+        {
+            var ageInYears = Int32.Parse(Console.ReadLine());
+            
+            if (ageInYears < 0) throw new Exception();
+
+            Console.WriteLine(
+                String.Format("This is your age in days: {0}", CalcAge(ageInYears))
+            );
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Input must be a valid number! Positive and not null.");
+        }
+    }
+
+    public static int CalcAge(int ageInYears)
+    {
+        const int YEAR_LENGTH = 365;
+
+        return ageInYears * YEAR_LENGTH;
+    }
+}
