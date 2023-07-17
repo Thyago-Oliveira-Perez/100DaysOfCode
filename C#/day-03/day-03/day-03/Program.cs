17e907d7fc4f3538dec92e601cbc1f1cd49aa5b0
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-07-17 14:30:22 -0300

    feat: c# day03

---
+++ C#/day-03/day-03/day-03/Program.cs
@@ -0,0 +1,41 @@
+﻿class Day03 
+{ 
+    public static void Main()
+    {
+        List<string> skewers = new() { 
+            "--oooo-ooo--",
+            "--xx--x--xx--",
+            "--o---o--oo--",
+            "--xx--x--ox--",
+            "--xx--x--ox--" 
+        };
+
+        int[] result = CountSkewers(skewers);
+
+        Console.WriteLine(string.Format("skewers\nvegetable: {0} non-vegetable: {1}", result[0], result[1]));
+    }
+
+    public static int[] CountSkewers(List<string> skewers)
+    {
+        int qtdV = 0;
+        int qtdM = 0;
+
+        foreach (string skewer in skewers) 
+        {
+            bool vegetable = skewer.Contains("o");
+            bool meat = skewer.Contains("x");
+
+            if (vegetable && meat || !vegetable && meat) 
+            { 
+                qtdM++;
+            }
+
+            if (vegetable && !meat)
+            {
+                qtdV++;
+            }
+        }
+        // [vegetarian skewers, non-vegetarian skewers]
+        return new int[] { qtdV, qtdM };
+    }
+}
