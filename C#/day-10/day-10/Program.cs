947e2432df7ae898f532f4b1c9e6e555ec223b3f
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-07-29 14:30:22 -0300

    feat: c# day10

---
+++ C#/day-10/day-10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Numerics;
+
+class Day10
+{
+    public static void Main()
+    {
+        Console.WriteLine($"Output: {CalculateCatalanNumber(3)}");
+    }
+
+    public static int CalculateCatalanNumber(int n)
+    {
+        int numerator = Factorial(2 * n);
+        int denominator = Factorial(n + 1) * Factorial(n);
+
+        return numerator / denominator;
+    }
+
+    private static int Factorial(int n)
+    {
+        if (n <= 1)
+        {
+            return 1;
+        }
+
+        int result = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+}
