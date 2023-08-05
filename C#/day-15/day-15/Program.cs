76f7399e323b860b85178c71cc28631d37a77c84
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-08-05 14:30:22 -0300

    feat: C# day15

---
+++ C#/day-15/day-15/Program.cs
@@ -0,0 +1,29 @@
+﻿class Day15
+{
+    public static void Main()
+    {
+        Console.WriteLine(IsAnagram("anagram", "nagaram"));
+        Console.WriteLine(IsAnagram("rat", "car"));
+    }
+
+    public static bool IsAnagram(string word, string word2)
+    {
+        if (word.Length != word2.Length) return false;
+
+        var wordArray = word.ToCharArray();
+        Array.Sort(wordArray);
+
+        var word2Array = word2.ToCharArray();
+        Array.Sort(word2Array);
+
+        for (var i = 0; i < wordArray.Length; i++)
+        {
+            if (wordArray[i] != word2Array[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
