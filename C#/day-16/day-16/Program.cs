d216317a5f88bdca9900ebcc28a3892edd75293d
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-08-06 14:30:22 -0300

    feat: c# day17

---
+++ C#/day-16/day-16/Program.cs
@@ -0,0 +1,18 @@
+﻿class Day16
+{
+    public static void Main()
+    {
+        Console.WriteLine(NimGame(1)); // should win
+        Console.WriteLine(NimGame(2)); // should win
+        Console.WriteLine(NimGame(4)); // should lose
+        Console.WriteLine(NimGame(5)); // should lose
+        Console.WriteLine(NimGame(6)); // should lose
+        Console.WriteLine(NimGame(7)); // should win
+        Console.WriteLine(NimGame(10)); // should lose
+    }
+
+    public static bool NimGame(int numStones)
+    {
+        return (int)Math.Ceiling((double)numStones / 3) % 2 != 0;
+    }
+}
